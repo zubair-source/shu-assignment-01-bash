246e3a635d27fec7602bdf93995a2af183ca8f29
Gulraeez Gulshan <44838822+gulraeezgulshan@users.noreply.github.com>
2023-03-17 12:47:51 +0500

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #   Write a bash script such that:
-#+  1) The script exits if it is not a root user
-#+  2) The script zips all the scipts files with .sh extention in current directory
+#+  1) The script exits if user is not a root.
+#+  2) The script zips all the scipts files with .sh extention in current directory.
 #+  3) The name of zip file should be same as script name.
