9184c9d78ea77c19e3c3e5435f35b7a2cb5cf248
Gulraeez Gulshan <44838822+gulraeezgulshan@users.noreply.github.com>
2023-03-17 12:43:11 +0500

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #   Write a bash script such that:
 #+  1) The script should exit with error code if the user is not a root user.
-#+  2) It switch to /var/log directory.
-#+  3) Delete the content of syslog file inside /var/log. 
-#+  4) Delete the content of wtmp file inside /var/log. 
-#+  5) The script should show how many lines in the syslog file it cleared.  
+#+  2) The script switches to /var/log directory.
+#+  3) If the directory does not exsists, it exits.
+#+  4) The script deletes the content of syslog file inside /var/log. 
+#+  5) The script delete the content of wtmp file inside /var/log. 
+#+  6) The script echoes the number of lines in the syslog and wtmp file it cleared.  
