747c214ce0becced9d376ca65b5dd92cd4f5b3f2
Gulraeez Gulshan <44838822+gulraeezgulshan@users.noreply.github.com>
2023-03-17 12:41:09 +0500

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #   Write a bash script such that:
-#+  1) It install git in a system, if it not already installed
-#+  2) The script can only be run by root user 
+#+  1) It installs git in a system, if it not already installed.
+#+  2) The script exits if the user is not a root.
+#+  3) The script echoes the version of git installed.
