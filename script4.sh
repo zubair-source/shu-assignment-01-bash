bfc3a92004d6d0da869cf0d14d9b75f855e72750
Gulraeez Gulshan <44838822+gulraeezgulshan@users.noreply.github.com>
2023-03-17 12:47:07 +0500

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #   Write a bash script such that:
-#   NOTE: Test the script below, to other directory. not in this directory to avoid duplicate
-#+  git repositories. If you don't understand this, talk to me before execting task (Gulraiz)    
 
-#+  1) The script exits if it is not a root user
+#+   NOTE: Test this script to other directory. not in this directory to avoid duplicate
+#+  git repositories (git inside git). If you don't understand this, talk to me before 
+#+  executing this task (Gulraiz)    
+
+#+  1) The script exits if it is not a root user.
 #+  2) The script is run inside a directory which is to tested.
-#+  3) The script test if the current directory is already a git repository, it exits.
+#+  3) The script tests if the current directory is already a git repository, it exits.
 #+  4) The script takes the input from the user as commit message.
 #+  5) The script exits if no parameter is provided.
 #+  6) The script initialize the git repositoy.
